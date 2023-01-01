219b401d5f7b4444943dc5f39bfc5fd854e2145c
KingNelx <tapiajonel606@gmail.com>
2023-01-01 18:14:57 +0800

    added useState and useEffect for all tables

--- src/Components/AllBirds.jsx
+++ src/Components/AllBirds.jsx
@@ -1,7 +1,29 @@
+import { Link } from "react-router-dom";
+
 const AllBirds = () => {
     return (
-        <div>
-
+        <div className="container mt-5">
+            <h2 className="mb-5">List of Birds</h2>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Pet Name</th>
+                        <th scope="col">Pet Owner</th>
+                        <th scope="col">Pet Age</th>
+                        <th scope="col">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                </tbody>
+            </table>
+            <Link to="/">Go Back</Link>
         </div>
     );
 }
--- src/Components/AllCats.jsx
+++ src/Components/AllCats.jsx
@@ -1,9 +1,31 @@
+import { Link } from "react-router-dom";
+
 const AllCats = () => {
-    return ( 
-        <div>
-            
+    return (
+        <div className="container mt-5">
+            <h2 className="mb-5">List of Cats</h2>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Pet Name</th>
+                        <th scope="col">Pet Owner</th>
+                        <th scope="col">Pet Age</th>
+                        <th scope="col">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                </tbody>
+            </table>
+            <Link to="/">Go Back</Link>
         </div>
-     );
+    );
 }
- 
+
 export default AllCats;
