d215b6aaeb8b1cd3140a8e5c6aa2552d6a4e5898
KingNelx <tapiajonel606@gmail.com>
2023-02-23 01:51:09 +0800

    input pet names in each cards

--- src/Components/OTHERS/PetCols.jsx
+++ src/Components/OTHERS/PetCols.jsx
@@ -2,12 +2,12 @@ const PetCols = () => {
   return (
     <div>
       <div class="container text-center mt-5">
-        <div class="row">
-          <div class="col">
+        <div class="row mx-3">
+          <div class="col mx-3">
             <div class="card" style={{ width: "18rem" }}>
               <img src="..." class="card-img-top" alt="..." />
               <div class="card-body">
-                <h5 class="card-title">Card title</h5>
+                <h5 class="card-title">DOGS</h5>
                 <p class="card-text">
                   Some quick example text to build on the card title and make up
                   the bulk of the card's content.
@@ -22,7 +22,7 @@ const PetCols = () => {
             <div class="card" style={{ width: "18rem" }}>
               <img src="..." class="card-img-top" alt="..." />
               <div class="card-body">
-                <h5 class="card-title">Card title</h5>
+                <h5 class="card-title">CATS</h5>
                 <p class="card-text">
                   Some quick example text to build on the card title and make up
                   the bulk of the card's content.
@@ -37,7 +37,7 @@ const PetCols = () => {
             <div class="card" style={{ width: "18rem" }}>
               <img src="..." class="card-img-top" alt="..." />
               <div class="card-body">
-                <h5 class="card-title">Card title</h5>
+                <h5 class="card-title">RABBITS</h5>
                 <p class="card-text">
                   Some quick example text to build on the card title and make up
                   the bulk of the card's content.
@@ -52,12 +52,12 @@ const PetCols = () => {
       </div>
 
       <div class="container text-center mt-5">
-        <div class="row">
-          <div class="col">
+        <div class="row mx-3">
+          <div class="col mx-3">
             <div class="card" style={{ width: "18rem" }}>
               <img src="..." class="card-img-top" alt="..." />
               <div class="card-body">
-                <h5 class="card-title">Card title</h5>
+                <h5 class="card-title">TURTLES</h5>
                 <p class="card-text">
                   Some quick example text to build on the card title and make up
                   the bulk of the card's content.
@@ -72,7 +72,7 @@ const PetCols = () => {
             <div class="card" style={{ width: "18rem" }}>
               <img src="..." class="card-img-top" alt="..." />
               <div class="card-body">
-                <h5 class="card-title">Card title</h5>
+                <h5 class="card-title">SNAKES</h5>
                 <p class="card-text">
                   Some quick example text to build on the card title and make up
                   the bulk of the card's content.
@@ -87,7 +87,7 @@ const PetCols = () => {
             <div class="card" style={{ width: "18rem" }}>
               <img src="..." class="card-img-top" alt="..." />
               <div class="card-body">
-                <h5 class="card-title">Card title</h5>
+                <h5 class="card-title">BIRDS</h5>
                 <p class="card-text">
                   Some quick example text to build on the card title and make up
                   the bulk of the card's content.
