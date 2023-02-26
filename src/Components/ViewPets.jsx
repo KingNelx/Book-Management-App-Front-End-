d975e90e35e1c89a17296b8f34bc6ac6b00da896
KingNelx <tapiajonel606@gmail.com>
2023-02-26 15:26:59 +0800

    remove un-used links

--- src/Components/ViewPets.jsx
+++ src/Components/ViewPets.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import "../CustomCSS/Scroll.css";
-import { Link } from "react-router-dom";
 
 const ViewPets = () => {
   const [pet, setPet] = useState([]);
