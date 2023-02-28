73bb856d20c818b22958887b5b91c519287e130b
KingNelx <tapiajonel606@gmail.com>
2023-02-28 21:58:48 +0800

    added carousel

--- src/Components/Start.jsx
+++ src/Components/Start.jsx
@@ -1,15 +1,58 @@
-import petLogo from "../Images/pet-logo.png";
+import catC from "../Components/Carousel/cat-c.jpg"
+import dogC from "../Components/Carousel/dog-c.jpg"
+import rabbitC from "../Components/Carousel/rabbit-c.jpg"
+import turtleC from "../Components/Carousel/turtle-c.jpg"
+
 import { Link } from "react-router-dom";
 import "../CustomCSS/Style.css";
 import Title from "./Title";
 const Start = () => {
   return (
     <>
-    <Title />
+      <Title />
       <div class="container text-center mt-5">
         <div class="row">
           <div class="col mt-5">
-            <img src={petLogo} alt="pet-logo" className="pet-logo" />
+            <div id="carouselExampleFade" class="carousel slide carousel-fade">
+              <div class="carousel-inner img-c">
+                <div class="carousel-item active ">
+                  <img src={catC} class="d-block w-100" alt="..." />
+                </div>
+                <div class="carousel-item">
+                  <img src={dogC} class="d-block w-100" alt="..." />
+                </div>
+                <div class="carousel-item">
+                  <img src={rabbitC} class="d-block w-100" alt="..." />
+                </div>
+                <div class="carousel-item">
+                  <img src={turtleC} class="d-block w-100" alt="..." />
+                </div>
+              </div>
+              <button
+                class="carousel-control-prev"
+                type="button"
+                data-bs-target="#carouselExampleFade"
+                data-bs-slide="prev"
+              >
+                <span
+                  class="carousel-control-prev-icon"
+                  aria-hidden="true"
+                ></span>
+                <span class="visually-hidden">Previous</span>
+              </button>
+              <button
+                class="carousel-control-next"
+                type="button"
+                data-bs-target="#carouselExampleFade"
+                data-bs-slide="next"
+              >
+                <span
+                  class="carousel-control-next-icon"
+                  aria-hidden="true"
+                ></span>
+                <span class="visually-hidden">Next</span>
+              </button>
+            </div>
           </div>
           <div class="col mt-5">
             <h1 className="py-5 my-5">
