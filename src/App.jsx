53ebf2c10f24c309884864820a7b9450c89de1db
KingNelx <tapiajonel606@gmail.com>
2023-03-08 08:54:46 +0800

    remake front end

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,17 @@
 import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Start from './Components/Start';
-import Home from './Components/Home';
-import Others from './Components/OTHERS/Others';
-import Dogs from './Components/DOGS/Dogs';
-import Cats from './Components/CATS/Cats';
-import Rabbits from './Components/RABBITS/Rabbits';
-import Snakes from './Components/SNAKES/Snakes';
-import Birds from './Components/BIRDS/Birds';
-import Turtles from './Components/TURTLES/Turtles';
-import UpdateData from './Components/UpdateData';
+
 
 const App = () => {
   return ( 
-   <Router>
-    <Routes>
-      <Route path="/" element={<Start />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/otherPets" element={<Others />} />
-      <Route path="/dogs" element={<Dogs />} />
-      <Route path="/cats" element={<Cats />}/>
-      <Route path="/rabbits" element={<Rabbits />} />
-      <Route path="/snakes" element={<Snakes />} />
-      <Route path="/birds" element={<Birds />}/>
-      <Route path="/turtles" element={<Turtles />}/>
-      <Route path="/updateData/:id" element={<UpdateData />} />
-    </Routes>
-   </Router>
+  //  <Router>
+  //   <Routes>
+     
+  //   </Routes>
+  //  </Router>
+  <div>
+    TEST
+  </div>
    );
 }
  
